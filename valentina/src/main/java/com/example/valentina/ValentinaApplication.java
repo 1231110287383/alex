a23c6e5f00a8d4bbf4758e469018ad9cedb105be
valentina <camilantina@gmail.com>
2022-09-16 11:33:12 -0500

    spring boot

---
+++ valentina/src/main/java/com/example/valentina/ValentinaApplication.java
@@ -0,0 +1,13 @@
+package com.example.valentina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValentinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValentinaApplication.class, args);
+	}
+
+}
